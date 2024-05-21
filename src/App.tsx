ae09a0830703e9c1c554f88da884483c9c1bd6eb
EijiGorilla <emasuz@hotmail.com>
2024-05-21 14:23:46 +0800

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,50 @@ import React, {
   useContext,
   useRef,
   useMemo,
+  useCallback,
 } from 'react';
 import Select from 'react-select';
 import { GeoJsonLayer } from '@deck.gl/layers';
 import type { GeoJsonLayerProps } from '@deck.gl/layers';
 import './App.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, { Source, Layer, MapRef } from 'react-map-gl';
+import Map, { Source, Layer, MapRef, Popup } from 'react-map-gl';
 import type { CircleLayer } from 'react-map-gl';
 import { MapViewState } from '@deck.gl/core';
 import type { FeatureCollection } from 'geojson';
 import land_local from './data.json';
 import { DeckGL } from 'deck.gl';
-import { dataLayerFill, dataLayerLine } from './map-style';
+import {
+  lotPolyStyle,
+  dataLayerLine,
+  lotPtStyle,
+  lotPtLabel,
+  stationLabel,
+  stationStyle,
+} from './map-style';
 import { uniqueValue, addDropdown } from './Query';
 import { LngLatBounds } from 'react-map-gl';
 import { features } from 'process';
 import bbox from '@turf/bbox';
+import bboxPolygon from '@turf/bbox-polygon';
+import center from '@turf/center';
+import { Label } from '@amcharts/amcharts5';
+import { any } from '@amcharts/amcharts5/.internal/core/util/Array';
 
 // Mapbox for React: https://visgl.github.io/react-map-gl/
 
 function App() {
+  let emptyFeatureCollection: any = { type: 'FeatureCollection', features: [] };
+  let emptyFeatureCollection1: any = { type: 'FeatureCollection', features: [] };
   const mapRef = useRef<MapRef | undefined | any>();
   const [initStations, setInitStations] = useState<null | undefined | any>();
   const [stationSelected, setStationSelected] = useState<null | any>(null);
   const [allData, setAllData] = useState<FeatureCollection>();
-  const [initialData, setInitialData] = useState<FeatureCollection>();
-  const [testClicked, setTestClicked] = useState<boolean>(false);
-
-  // const layer = new GeoJsonLayer<PropertiesType>({
-  //   id: 'GeoJsonLayer',
-  //   data: 'https://raw.githubusercontent.com/EijiGorilla/EijiGorilla.github.io/master/WebApp/ArcGIS_API_for_JavaScript/Sample/MMSP_Land_Sample.geojson',
-  //   filled: true,
-  // });
-  //
+  const [pointData, setPointData] = useState<FeatureCollection>();
+  const [clickedInfo, setClickedInfo] = useState<any>(null);
+  const [clickedBool, setClickedBool] = useState<boolean>(false);
+  const [filteredGeojson, setFilteredGeojson] = useState<FeatureCollection>(emptyFeatureCollection);
+  const [labelGeojson, setLabelGeojson] = useState<FeatureCollection>(emptyFeatureCollection1);
 
   // Load Geojson Data
   useEffect(() => {
@@ -49,7 +59,6 @@ function App() {
       .then((resp) => resp.json())
       .then((json) => {
         setAllData(json);
-        setInitialData(json);
         // Create a dropdown list
         let station_all: any = [];
         json.features.map((property: any, index: any) => {
@@ -64,52 +73,120 @@ function App() {
         });
         const final = addDropdown(dropdownArray, { field1: 'All' });
         setInitStations(final);
+
+        // Label
+        const labelPointFeature = json.features.map((feature: any, index: any) => {
+          // get centroid of each polygon
+          const bound_box = bbox(feature);
+          const poly = bboxPolygon(bound_box);
+          const {
+            geometry: { coordinates: coordinatesCenter },
+          } = center(poly);
+          const value = coordinatesCenter.map((num: any) => Number(num));
+
+          // get property values
+          const cn_number = feature.properties['CN'];
+          const station_name = feature.properties['Station1'];
+          return Object.assign({
+            type: 'Feature',
+            id: index + 1,
+            geometry: { type: 'Point', coordinates: value },
+            properties: { Id: cn_number, Station1: station_name },
+          });
+        });
+
+        labelGeojson.features = labelPointFeature;
+      })
+      .catch((err) => console.error('Could not load data', err));
+  }, []);
+
+  useEffect(() => {
+    fetch(
+      'https://raw.githubusercontent.com/EijiGorilla/EijiGorilla.github.io/master/WebApp/ArcGIS_API_for_JavaScript/Sample/MMSP_Station.geojson',
+    )
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPointData(json);
       })
       .catch((err) => console.error('Could not load data', err));
   }, []);
 
   const data = useMemo(() => {
-    return allData;
-  }, [allData]);
+    return [allData, pointData, labelGeojson];
+  }, [allData, pointData, labelGeojson]);
 
   // Dropdown filter
   const handleMunicipalityChange = (obj: any) => {
     setStationSelected(obj);
   };
 
+  // Popup
+  const clickedOn = useCallback(
+    (event: any) => {
+      const {
+        features,
+        point: { x, y },
+      } = event;
+      const clickedFeature = features && features[0];
+      setClickedBool(clickedBool === false ? true : true);
+
+      setClickedInfo(clickedFeature && { feature: clickedFeature, x, y });
+    },
+    [clickedBool],
+  );
+
   // Filter
   const filter = useMemo(
     () => ['in', 'Station1', stationSelected && stationSelected.field1],
     [stationSelected],
   );
-
-  // Id field is NOT empty
   const filter_all = useMemo(() => ['!=', 'Id', true], [stationSelected]);
 
+  // good sample for filtering
+  // https://labs.mapbox.com/impact-tools/finder/
   // Zoom to bounds
+  function zoomToLayer(search_property: any, zoom: any, data: any) {
+    data[0]?.features.forEach((feature: any) => {
+      // if (feature.properties['Station1'].includes('Tandang Sora')) { // includes keyword
+      //   console.log(feature);
+      // }
+      if (feature.properties[search_property] === stationSelected.field1) {
+        filteredGeojson.features.push(feature);
+      }
+    });
+
+    const [minLng, minLat, maxLng, maxLat] = bbox(filteredGeojson);
+
+    mapRef.current.fitBounds(
+      [
+        [minLng, minLat],
+        [maxLng, maxLat],
+      ],
+      { padding: 40, duration: 1000, zoom: zoom },
+    );
+  }
+
   useEffect(() => {
-    if (stationSelected) {
-      // const [minLng, minLat, maxLng, maxLat] = bbox(allData?.features[0]);
-      const feature = allData?.features[0].geometry;
-      const [minLng, minLat, maxLng, maxLat] = bbox(feature);
-
-      console.log(minLat);
+    if (stationSelected && stationSelected.field1 !== 'All') {
+      // Reset filtered geojson layer
+      setFilteredGeojson(emptyFeatureCollection);
+      zoomToLayer('Station1', 16, data);
+    } else if (stationSelected && stationSelected.field1 === 'All') {
+      setFilteredGeojson(emptyFeatureCollection);
+      data[0]?.features.forEach((feature: any) => {
+        filteredGeojson.features.push(feature);
+      });
+      const [minLng, minLat, maxLng, maxLat] = bbox(filteredGeojson);
+      mapRef.current.fitBounds(
+        [
+          [minLng, minLat],
+          [maxLng, maxLat],
+        ],
+        { padding: 100, duration: 1000 },
+      );
     }
-
-    // const feature = event.features[0];
-
-    // // calculate the bounding box of the feature
-    // const [minLng, minLat, maxLng, maxLat] = bbox(feature);
-
-    // mapRef.current.fitBounds(
-    //   [
-    //     [minLng, minLat],
-    //     [maxLng, maxLat],
-    //   ],
-    //   { padding: 40, duration: 1000 },
-    // );
   }, [stationSelected]);
-
+  // test
   // Style CSS
   const customstyles = {
     option: (styles: any, { data, isDisabled, isFocused, isSelected }: any) => {
@@ -166,17 +243,49 @@ function App() {
           latitude: 14.6821565,
           zoom: 14,
         }}
-        interactiveLayerIds={['data', 'dataLine']}
+        onClick={clickedOn}
+        interactiveLayerIds={['lotPoly', 'stationPt', 'stationPtLabel', 'lotPt', 'lotPtLabels']}
         mapStyle={'mapbox://styles/mapbox/streets-v9'}
       >
         {/* why use useMemo? */}
-        <Source type="geojson" data={data}>
-          <Layer {...dataLayerFill} />
+        <Source type="geojson" data={data[0]}>
+          <Layer {...lotPolyStyle} id="lotPoly" />
           <Layer
-            {...dataLayerFill}
+            {...lotPolyStyle}
             filter={stationSelected && stationSelected.field1 === 'All' ? filter_all : filter}
+            id="lotPoly"
           />
         </Source>
+        <Source type="geojson" data={data[1]}>
+          <Layer {...stationStyle} id="stationPt" />
+          <Layer {...stationLabel} id="stationPtLabel" />
+        </Source>
+        <Source type="geojson" data={data[2]}>
+          <Layer {...lotPtStyle} id="lotPt" />
+          <Layer
+            {...lotPtLabel}
+            filter={
+              stationSelected && stationSelected.field1 !== 'All'
+                ? ['in', 'Station1', stationSelected.field1]
+                : filter_all
+            }
+            id="lotPtLabels"
+          />
+        </Source>
+
+        {clickedInfo && (
+          <div
+            className="w-30 h-10 absolute z-99 bg-white p-3 pt-1"
+            style={{ left: clickedInfo.x, top: clickedInfo.y }}
+          >
+            <div>
+              Station: <b>{clickedInfo.feature.properties.Station1}</b>
+            </div>
+            <div>
+              Lot Id: <b>{clickedInfo.feature.properties.Id}</b>
+            </div>
+          </div>
+        )}
       </Map>
     </div>
   );
--- src/map-style.ts
+++ src/map-style.ts
@@ -1,8 +1,9 @@
-import type { FillLayer, LineLayer } from 'mapbox-gl';
+import type { FillLayer, LineLayer, Layer, SymbolLayer, CircleLayer } from 'mapbox-gl';
 import { statusLotColor } from './statusUniqueValues';
 
 // For more information on data-driven styles, see https://www.mapbox.com/help/gl-dds-ref/
-export const dataLayerFill: FillLayer = {
+// Lot polygon style
+export const lotPolyStyle: FillLayer = {
   id: 'data',
   type: 'fill',
   //   filter: ['==', 'class', 'park'],
@@ -31,5 +32,73 @@ export const dataLayerLine: LineLayer = {
   layout: {},
   paint: {
     'line-width': 3,
+  },
+};
+
+// Station point style
+export const stationLabel: SymbolLayer = {
+  id: 'labelTextLayer',
+  type: 'symbol',
+  // source: textGeoJsonSource,
+  paint: {
+    'text-color': '#0000FF', //Color of your choice
+    'text-halo-blur': 5,
+    'text-halo-color': '#ffffff',
+    'text-halo-width': 1,
+    'text-opacity': 1,
+  },
+  layout: {
+    'text-field': ['get', 'Station'], //This will get "t" property from your geojson
+    // 'text-font': textFontFamily,
+    'text-rotation-alignment': 'auto',
+    'text-allow-overlap': true,
+    'text-anchor': 'top',
+    'text-size': 12,
+    'text-line-height': 1,
+    'text-offset': [0, 0],
+  },
+};
+
+export const stationStyle: CircleLayer = {
+  id: 'data',
+  type: 'circle',
+  //   filter: ['==', 'class', 'park'],
+  paint: {
+    'circle-color': '#000000',
+    'circle-radius': 2,
+  },
+};
+
+// Lot labels using point
+export const lotPtStyle: CircleLayer = {
+  id: 'labelPoint',
+  type: 'circle',
+  //   filter: ['==', 'class', 'park'],
+  paint: {
+    'circle-color': '#000000',
+    'circle-radius': 0,
+  },
+};
+
+export const lotPtLabel: SymbolLayer = {
+  id: 'labelTextLayer',
+  type: 'symbol',
+  // source: textGeoJsonSource,
+  paint: {
+    'text-color': '#000000', //Color of your choice
+    // 'text-halo-blur': 5,
+    // 'text-halo-color': '#ffffff',
+    // 'text-halo-width': 1,
+    'text-opacity': 1,
+  },
+  layout: {
+    'text-field': ['get', 'Id'], //This will get "t" property from your geojson
+    // 'text-font': textFontFamily,
+    'text-rotation-alignment': 'auto',
+    'text-allow-overlap': true,
+    'text-anchor': 'top',
+    'text-size': 10,
+    'text-line-height': 1,
+    'text-offset': [0, 0],
   },
 };
